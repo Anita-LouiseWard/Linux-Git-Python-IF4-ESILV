f5593ce4af7988a5b86b5cf501093f983a71173c
root <root@ip-172-31-53-149.ec2.internal>
2023-03-24 15:54:46 +0000

    test git

--- scraping.sh
+++ scraping.sh
@@ -12,4 +12,4 @@ INFORMATION=$(echo "$CONTENT" | grep "hours" | grep "minutes" | grep "seconds" |
 HORAIRE=$(echo "$INFORMATION" | grep -o "[0-9][0-9]:[0-9][0-9]:[0-9][0-9]" | sed 's/://g')
 
 # Affichage des informations récupérées
-echo "Les valeurs de l'heure son : $HORAIRE"
+echo "Les valeurs de l'heure sont : $HORAIRE"
