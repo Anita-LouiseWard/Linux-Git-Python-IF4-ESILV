91eefb8e4c02b6303b922ea0a02fcb47633cec63
Anita-LouiseWard <128745470+Anita-LouiseWard@users.noreply.github.com>
2023-03-24 13:45:48 +0100

    Update scraping

--- scraping.sh
+++ scraping.sh
@@ -7,9 +7,9 @@ URL="https://fr.timeservers.net/cities/fr/paris"
 CONTENT=$(curl -s "$URL")
 
 # Récupération des informations souhaitées en utilisant grep et sed
-INFORMATION=$(echo "$CONTENT" | grep "hours" | grep "minutes" | grep "seconds" >
+INFORMATION=$(echo "$CONTENT" | grep "hours" | grep "minutes" | grep "seconds" | sed 's/<[^>]*>//g')
 
-HORAIRE=$(echo "$INFORMATION" | grep -o "[0-9][0-9]:[0-9][0-9]:[0-9][0-9]" | se>
+HORAIRE=$(echo "$INFORMATION" | grep -o "[0-9][0-9]:[0-9][0-9]:[0-9][0-9]" | sed 's/://g')
 
 # Affichage des informations récupérées
 echo "Les valeurs de l'heure son : $HORAIRE"
